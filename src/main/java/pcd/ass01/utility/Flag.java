2d15970a979f7687f6552536aaf5839e315e17f9
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-04-07 18:47:02 +0200

    replaced synchronized blocks with lock

--- src/main/java/pcd/ass01/utility/Flag.java
+++ src/main/java/pcd/ass01/utility/Flag.java
@@ -1,22 +1,42 @@
 package pcd.ass01.utility;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Flag {
 
 	private boolean flag;
+	private final Lock mutex;
 	
 	public Flag() {
 		this.flag = false;
+		this.mutex = new ReentrantLock();
 	}
 	
-	public synchronized void reset() {
-		this.flag = false;
+	public void reset() {
+		try {
+			this.mutex.lock();
+			this.flag = false;
+		} finally {
+			this.mutex.unlock();
+		}
 	}
 	
-	public synchronized void set() {
-		this.flag = true;
+	public void set() {
+		try {
+			this.mutex.lock();
+			this.flag = true;
+		} finally {
+			this.mutex.unlock();
+		}
 	}
 	
-	public synchronized boolean isSet() {
-		return this.flag;
+	public boolean isSet() {
+		try {
+			this.mutex.lock();
+			return this.flag;
+		} finally {
+			this.mutex.unlock();
+		}
 	}
 }
